b1d2b4c7a9f3baf39515846eee7b8a91347d636b
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-10-17 19:18:36 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] numberGrid =
+            {
+                {1, 2 },
+                {3, 4  },
+                {5, 6  }
+            };
+
+            Console.WriteLine(numberGrid[0, 0]);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
